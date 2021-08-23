febd59383afee7fb27168332b2cdb5cc434d4ecd
Qoosim <ayinde.abdulghaniyy@gmail.com>
2021-08-23 11:37:31 +0100

    Complete game for the terminal

--- script.js
+++ script.js
@@ -27,8 +27,33 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-const playerSelection = prompt('Make your selection: ');
-const computerSelection = computerPlay();
-console.log(computerSelection);
-console.log(playRound(playerSelection.toLowerCase(), computerSelection));
+//const playerSelection = prompt('Make your selection: ');
+//const computerSelection = computerPlay();
+//console.log(computerSelection);
+
+function game(times) {
+    let playerScores = 0;
+    let computerScores = 0;
+
+    for(let i = 1; i <= times; i++) {
+        const playerSelection = prompt('Make your selection: ');
+        const computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
+        let result = playRound(playerSelection, computerSelection).toLowerCase();
+        if (result.includes('you won')) {
+            playerScores += 1;
+        } else if (result.includes('computer won')) {
+            computerScores += 1;
+        }
+    }
+    if (playerScores > computerScores) {
+        console.log('Player won!.');
+    } else if(playerScores < computerScores) {
+        console.log('Computer won!.');
+    } else {
+        console.log('Game ended in tie!. Try again.');
+    }
+}
+
+game(5);
 
